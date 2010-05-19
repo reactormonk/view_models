5e53678b2595be1bd79e49452434e8943c2052b5
Andreas Schacke <andreas.schacke@gmail.com>
2010-05-19 16:18:27 +0200

    - removed method (override not needed anymore)

--- lib/view_models/view.rb
+++ lib/view_models/view.rb
@@ -6,7 +6,8 @@ module ViewModels
     # Include the helpers from the view model.
     #
     def initialize controller, master_helper_module
-      metaclass.send :include, master_helper_module
+      singleton_class.send :include, master_helper_module
+      #super nil, {}, controller
       super controller.class.view_paths, {}, controller
     end
     
@@ -16,11 +17,12 @@ module ViewModels
       render options.to_render_options
     end
 
-    # Finds the template in the view paths at the given path, with its format.
+    # Finds the template in the view paths at the given path, with its format.                                         
     #
-    def find_template path
-      view_paths.find_template path, template_format rescue nil
-    end
+    # def find_template path                                                                                             
+    #       view_paths.find_template path, template_format rescue nil                                                        
+    #     end
+    # not needed anymore
     
   end
 end
